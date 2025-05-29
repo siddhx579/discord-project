a998707980a1145ea704a9a38642fbb22f12ea38
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-05-29 12:25:32 +0530

    Update file-upload.tsx

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -22,6 +22,9 @@ export const FileUpload = ({
         return (
             <div className="relative h-20 w-20">
                 <Image fill src={value} alt="Upload" className="rounded-full" />
+                <button onClick={() => onChange("")} className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm" type="button">
+                    <X className="h-4 w-4" />
+                </button>
             </div>
         )
     }
