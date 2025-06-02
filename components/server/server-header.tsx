c6f9f791c97674835280bbd947bea69c191c0aef
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-06-02 11:54:06 +0530

    Update server-header.tsx

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -55,7 +55,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 )}
                 {isModerator && (
                     <DropdownMenuItem
-                        // onClick={() => onOpen("createChannel", { server })}
+                        onClick={() => onOpen("createChannel", { server })}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Create Channel
