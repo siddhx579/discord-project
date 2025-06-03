c46248d3e142a295fe200288f5e4794184c70fa5
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-06-03 10:18:29 +0530

    Update modal-provider.tsx

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,6 +7,9 @@ import { EditServerModal } from "../modals/edit-server-modal";
 import { MembersModal } from "../modals/members-modal";
 import { CreateChannelModal } from "../modals/create-channel-modal";
 import { LeaveServerModal } from "../modals/leave-server-modal";
+import { DeleteServerModal } from "../modals/delete-server-modal";
+import { DeleteChannelModal } from "../modals/delete-channel-modal";
+import { EditChannelModal } from "../modals/edit-channel-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -27,6 +30,9 @@ export const ModalProvider = () => {
             <MembersModal />
             <CreateChannelModal />
             <LeaveServerModal />
+            <DeleteServerModal />
+            <DeleteChannelModal />
+            <EditChannelModal />
         </>
     );
 }
