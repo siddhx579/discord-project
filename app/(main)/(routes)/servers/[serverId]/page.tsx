3394e4e9c47247b959e44431654fe4d4359e5274
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-06-04 16:25:01 +0530

    Update page.tsx

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,7 @@
 import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 interface ServerIdPageProps {
     params: {
@@ -8,15 +10,40 @@ interface ServerIdPageProps {
 }
 
 const ServerIdPage = async ({ params }: ServerIdPageProps) => {
-    // const profile = await currentProfile();
-    // if(!profile) {
-    //     return RedirectToSignIn;
-    // }
-    return (
-        <div>
-            Server ID
-        </div>
-    )
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return RedirectToSignIn;
+    }
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general",
+                },
+                orderBy: {
+                    createdAt: "asc",
+                },
+            },
+        },
+    });
+
+    const initialChannel = server?.channels[0];
+
+    if (initialChannel?.name !== "general") {
+        return null;
+    }
+
+    return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 }
 
 export default ServerIdPage;
